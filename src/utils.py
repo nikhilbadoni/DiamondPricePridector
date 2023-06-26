b99b1f3804377fd397bc2a4f77ae757f5648bfb1
Nikhil Badoni <nikhilbadoni@Nikhils-MacBook-Air.local>
2023-06-26 13:22:49 +0530

    app.py and Web Application

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,7 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj,file_obj)
     except Exception as e:
+        logging.info("Exception Occured in save_object function in utils.py")
         raise CustomException(e,sys)
     
 def evalute_models(x_train, y_train, x_test, y_test, models):
@@ -42,6 +43,15 @@ def evalute_models(x_train, y_train, x_test, y_test, models):
     
     except Exception as e:
         logging.info("Error Occured at evalute models function")
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info("Exception Occured in load_object function in utils.py")
         raise CustomException(e,sys)
 
     
