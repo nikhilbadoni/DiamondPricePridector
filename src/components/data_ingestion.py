1233b2c38a32b945678078675ce618543c5a1d99
Nikhil Badoni <nikhilbadoni@Nikhils-MacBook-Air.local>
2023-06-26 11:49:38 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -50,14 +50,4 @@ class DataIngestion:
         
 
 
-# Run Data Ingestion
-
-
-if __name__=="__main__":
-    obj= DataIngestion()
-    train_data_path, test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-
-
     
--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,29 @@ def save_object(file_path, obj):
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evalute_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report= {}
+        for i in range(len(models)):
+            model= list(models.values())[i]
+            # Train Model
+            model.fit(x_train, y_train)
+
+
+            # Predict Testing Data
+            y_test_pred= model.predict(x_test)
+
+            # get R2 Scores for Train and Test Data
+            # train_model_score= r2_score(y_train,y_train_pred)
+            test_model_score= r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]= test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.info("Error Occured at evalute models function")
+        raise CustomException(e,sys)
+
+    
